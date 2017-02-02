7cd908ada417986a0126eaed9a88d66b4623cba4
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-02-02 08:28:58 +0100

    add umountvm for mac

--- vag.py
+++ vag.py
@@ -48,3 +48,6 @@ args 	= sys.argv[1:]
 cmd 	= "vagrant " +  " ".join(args)
 # call vagrant with command args
 os.system(cmd)
+
+test = os.popen(vagrant ssh $1 -c "ip address show eth1" 2>/dev/null | sed -ne '/inet / s/\s\+inet \([^\/]\+\).*\r/\1/p')#
+print test
